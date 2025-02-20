143f3df90c388ebd58b1d6941235c32ca1279af3
Fade Tran <151425346+Phuongkooto@users.noreply.github.com>
2025-02-20 10:34:29 +0700

    Tru2soo

---
+++ Tru/src/tru/Tru.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tru;
+
+/**
+ *
+ * @author anhph
+ */
+public class Tru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 1;
+        int b= 2;
+        int c = b-a;
+        System.out.println(c);
+    }
+    
+}
